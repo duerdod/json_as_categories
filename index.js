4475740e6ab72dad359ad86ba85b3e0b7269f5a2
Erik Pehrers <erik.pehrers@jetshop.se>
2020-02-10 20:32:17 +0100

    With closures

--- index.js
+++ index.js
@@ -2,81 +2,35 @@ const fs = require('fs');
 
 fs.readFile('categories.json', (err, data) => createCategories(err, JSON.parse(data)));
 
-function createCategories(err, data) {
+function createLine(prev) {
+    let final = '';
+    function inner(_, current) {
+        const { Value } = current.names[0]
+        return final += `${Value}; ${current.id}; ${prev.id};\n`
+    }
+    return inner
+}
 
-    const categories = data.categories.reduce((final, lv1) => {
-        final += `${lv1.names[0].Value}; ${lv1.id}; ${-1};\n`
-        if (lv1.categories) {
-            lv1.categories.reduce((_, lv2) => {
-                final += `${lv2.names[0].Value}; ${lv2.id}; ${lv1.id};\n`
-                if (lv2.categories) {
-                    lv2.categories.reduce((_, lv3) => {
-                        final += `${lv3.names[0].Value}; ${lv3.id}; ${lv2.id};\n`
-                        return final
-                    }, '')
+function createCategories(e, data) {
+    let allCategories = data.categories.reduce(createLine({ id: -1 }), '')
+
+    data.categories.forEach(({ id, categories }) => {
+        // Lv2
+        if (categories) {
+            allCategories += categories.reduce(createLine({ id }), '')
+            categories.forEach(({ id, categories }) => {
+                // Lv3
+                if (categories) {
+                    allCategories += categories.reduce(createLine({ id }), '')
                 }
-                return final;
-            }, '')
+
+            })
         }
-        return final
-    }, '')
 
-    writeToJSon(categories)
-    return categories
+    })
+
+    console.log(allCategories)
+
+    // fs.writeFileSync('converted_subcategories_v2.csv', allCategories)
+    return allCategories
 }
-
-function writeToJSon(data) {
-    fs.writeFileSync('converted_subcategories.csv', data)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function createCategories(err, data) {
-
-    const categories = data.categories.reduce((final, lv1) => {
-        final += `${lv1.names[0].Value}; ${lv1.id}; ${-1};\n`
-        if (lv1.categories) {
-            lv1.categories.reduce((_, lv2) => {
-                final += `${lv2.names[0].Value}; ${lv2.id}; ${lv1.id};\n`
-                if (lv2.categories) {
-                    lv2.categories.reduce((_, lv3) => {
-                        final += `${lv3.names[0].Value}; ${lv3.id}; ${lv2.id};\n`
-                        return final
-                    }, '')
-                }
-                return final;
-            }, '')
-        }
-        return final
-    }, '')
-
-    writeToJSon(categories)
-    return categories
-}
-
-function writeToJSon(data) {
-    fs.writeFileSync('converted_subcategories.csv', data)
-}
-
-*/
